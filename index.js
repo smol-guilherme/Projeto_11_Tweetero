1f82653994d5151363ce332a6abaa3a7c0a852c2
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-06-16 09:16:59 -0300

    Feature: Added '/tweets' route. Feature: Added POST Method to '/tweets' route.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { handleSignUp } from "./src/scripts.js";
+import { handleSignUp, handleTweet, handleDisplayTweet } from "./src/scripts.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get('/tweets', (req, res) => {
-
+    res.send(handleDisplayTweet())
 });
 
 app.post('/sign-up', async(req, res) => {
@@ -15,6 +15,16 @@ app.post('/sign-up', async(req, res) => {
     try {
         handleSignUp(newUser)
         res.send('OK');
+    } catch {
+        res.send('Error')
+    }
+})
+
+app.post('/tweets', async(req, res) => {
+    const newTweet = req.body;
+    try {
+        handleTweet(newTweet)
+        res.send('OK')
     } catch {
         res.send('Error')
     }
--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 import users from "./users.js";
+import tweets from "./tweets.js"
 
 export function handleSignUp(obj) {
-    console.log(users)
     const isNameTaken = users.find((item) => {
         if(item.username === obj.username) {
             return true;
@@ -12,5 +12,41 @@ export function handleSignUp(obj) {
         return console.error('Usuário já existe');
     }
     users.push(obj)
+    console.log(users)
     return console.log('OK');
+}
+
+export function handleTweet(obj) {
+    tweets.push(obj)
+    console.log(tweets)
+    return console.log('OK');
+}
+
+export function handleDisplayTweet() {
+    const tweetData = []
+    let i = 0
+    let tweet;
+    let avatar;
+    console.log(tweets)
+    // while (i < 10 || i < tweets.length) {
+    //     tweet = tweets[i];
+    //     console.log(tweet)
+    //     avatar = users.find((user) => {
+    //         if(user.username === tweet.username) {
+    //             return user
+    //         }
+    //     });
+    //     tweetData.push({ ...tweet, avatar: avatar.avatar })
+    //     i++;
+    // }
+    // return tweetData;
+}
+
+function getAvatar(name) {
+    const avatar = users.find((user) => {
+        if(user.username === name) {
+            return user.avatar
+        }
+    })
+    return avatar;
 }
